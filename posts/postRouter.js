dd4a340afbb38cb3796c1c67a73dec229985b2ce
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-10 15:39:28 +0200

    Feature: added middleware to validate id in postRouter.js

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -13,12 +13,27 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', async (req, res) => {
-  try {
-    const post = await Posts.getById(req.params.id);
+router.get('/:id', validatePostId, async (req, res) => {
+  // try {
+  //   const post = await Posts.getById(req.params.id);
 
-    if (post) {
-      res.status(200).json(post);
+  //   if (post) {
+      res.status(200).json(req.post);
+  //   } else {
+  //     res.status(400).json({ message: 'Post id is not available !' });
+  //   }
+
+  // } catch (error) {
+  //   res.status(500).json({ errorMessage: 'The request failed !!!' });
+  // }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleteIt = await Posts.remove(req.params.id);
+
+    if (deleteIt > 0) {
+      res.status(200).json({ message: 'Post got deleted !' });
     } else {
       res.status(400).json({ message: 'Post id is not available !' });
     }
@@ -28,38 +43,34 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
-  try {
-    const deleteIt = await Posts.remove(req.params.id);
-    
-    if (deleteIt > 0) {
-      res.status(200).json({ message: 'Post got deleted !'});
-    } else {
-      res.status(400).json({ message: 'Post id is not available !' });
-    }
-    
-      } catch (error) {
-        res.status(500).json({ errorMessage: 'The request failed !!!' });
-      }
-});
-
 router.put('/:id', async (req, res) => {
   try {
     const update = await Posts.update(req.params.id, req.body);
-    if(req.body.text[4]) {
-    res.status(200).json({ message: 'Post got updated !'});
+    if (req.body.text[4]) {
+      res.status(200).json({ message: 'Post got updated !' });
     } else {
-      res.status(400).json({ message: 'Text must be at least 5 characters long !'})
+      res.status(400).json({ message: 'Text must be at least 5 characters long !' })
     }
-      } catch (error) {
-        res.status(500).json({ errorMessage: 'The request failed !!!' });
-      }
+  } catch (error) {
+    res.status(500).json({ errorMessage: 'The request failed !!!' });
+  }
 });
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-
+async function validatePostId(req, res, next) {
+  const { id } = req.params
+  const post = await Posts.getById(id);
+  try {
+    if (post) {
+      req.post = post;
+      next();
+    } else {
+      res.status(404).json({ message: 'invalid Id ' })
+    }
+  } catch (error) {
+    res.status(500).json({ errorMessage: 'request dont could process' })
+  }
 };
 
 module.exports = router;
