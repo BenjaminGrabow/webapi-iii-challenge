28f48f0b457bd22ec8229adaef9bbb1b577dcea3
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-10 14:24:56 +0200

    Feature: added on all methods in userRouter asnyc and try and catch with error response

--- users/userRouter.js
+++ users/userRouter.js
@@ -5,7 +5,8 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    const user = await User.get(req.query)
+    const user = await User.get(req.query);
+
     res.status(200).json(user)
   } catch (error) {
     res.status(500).json({ errorMessage: 'The request failed !!!' })
@@ -26,25 +27,45 @@ res.status(200).json(user);
   }
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', async (req, res) => {
+try {
 
+} catch (error) {
+  res.status(500).json({ errorMessage: 'The request failed !!!' })
+}
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
 
+  } catch (error) {
+    res.status(500).json({ errorMessage: 'The request failed !!!' })
+  }
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', async (req, res) => {
+  try {
 
+  } catch (error) {
+    res.status(500).json({ errorMessage: 'The request failed !!!' })
+  }
 });
 
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
 
+  } catch (error) {
+    res.status(500).json({ errorMessage: 'The request failed !!!' })
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
 
+  } catch (error) {
+    res.status(500).json({ errorMessage: 'The request failed !!!' })
+  }
 });
 
 //custom middleware
