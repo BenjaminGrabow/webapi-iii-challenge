bb9c9fc97473f58a97a1a8729e3fd36c1b82906a
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-11 12:57:38 +0200

    Fix bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server.js');
 
-const port = proces.env.PORT || 3500;
+const port = process.env.PORT || 3500;
 
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
