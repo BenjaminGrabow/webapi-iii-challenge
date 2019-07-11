65195b00f0c3c265434eff563bb5fb35bfc94cf4
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-11 13:33:11 +0200

    Feature: added flexible port and start script for heroku

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require('./server');
 
-server.listen(3500, () => {
-  console.log('Server is running on http://localhost:3500')
+const port = process.env.PORT || 3500;
+
+server.listen((port), () => {
+  console.log(`Server is running on http://localhost:${port}`)
 });
